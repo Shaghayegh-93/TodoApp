31645fb9b1c54b83665092fb4d393643ecc68346
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-12-21 18:25:58 +0330

    import todoList component and render it

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,12 +1,14 @@
 import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 
 const TodoApp = () => {
-    return ( 
-        <div className="todoApp">
-            <h1>todos</h1>
-            <TodoForm/>
-        </div>
-     );
-}
- 
+  return (
+    <div className="todoApp">
+      <h1>todos</h1>
+      <TodoForm />
+      <TodoList />
+    </div>
+  );
+};
+
 export default TodoApp;
