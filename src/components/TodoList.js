7c0174d2c953f8ec651cfb5c12f7d112ad9ae96b
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-12-22 13:13:47 +0330

    add toggleAllTodo function

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,9 @@
-import { useTodoList } from "./Context/TodoListProvider";
+import { useTodoList, useTodoListAction } from "./Context/TodoListProvider";
 
 const TodoList = () => {
   const { todoList, filter } = useTodoList();
+
+  const dispatch = useTodoListAction();
   const getVisibleTodos = () => {
     switch (filter) {
       case "uncompleted": {
@@ -14,11 +16,23 @@ const TodoList = () => {
         return todoList;
     }
   };
+  const isAllTodosSelected = todoList.every((todo) => todo.isCompleted);
+  const onToggleAllTodos = (e) => {
+    dispatch({ type: "TOGGLE_ALL", payload: e.target.checked });
+  };
   const visibleTodos = getVisibleTodos();
   const noTodosClass = visibleTodos.length === 0 ? "hidden" : "";
 
   return (
     <section className={`main ${noTodosClass}`}>
+      <input
+        id="toggleAll"
+        className="toggleAll"
+        type="checkbox"
+        checked={isAllTodosSelected}
+        onChange={onToggleAllTodos}
+      />
+      <label htmlFor="toggleAll">mark all as completed</label>
       <ul className="todoList">
         {visibleTodos.map((todo) => {
           return <li key={todo.id}>{todo.text}</li>;
