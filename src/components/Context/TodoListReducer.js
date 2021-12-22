9d76c1c76cb427346a074345ddf36a29dbd44080
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-12-22 13:14:59 +0330

    add TOGGLE_ALL case to reducer function

--- src/components/Context/TodoListReducer.js
+++ src/components/Context/TodoListReducer.js
@@ -1,15 +1,21 @@
 const todoListReducer = (state, action) => {
   switch (action.type) {
-    case "ADD_TODO":
-      {
-        const newTodo = {
-          id: Math.ceil(Math.random() * 100),
-          text: action.payload,
-          isCompleted: false,
-        };
-        return { ...state, todoList: [...state.todoList, newTodo] };
-      }
-    
+    case "ADD_TODO": {
+      const newTodo = {
+        id: Math.ceil(Math.random() * 100),
+        text: action.payload,
+        isCompleted: false,
+      };
+      return { ...state, todoList: [...state.todoList, newTodo] };
+    }
+    case "TOGGLE_ALL": {
+      const filterTodos = state.todoList.map((todo) => ({
+        ...todo,
+        isCompleted: action.payload,
+      }));
+      return { ...state, todoList: filterTodos };
+    }
+
     default:
       return state;
   }
