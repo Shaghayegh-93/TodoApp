953c70939ced7db476b9a8335b8f91a8b1aa65af
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-12-25 11:11:32 +0330

    add five new cases for our dispath

--- src/components/Context/TodoListReducer.js
+++ src/components/Context/TodoListReducer.js
@@ -18,6 +18,39 @@ const todoListReducer = (state, action) => {
     case "CHANGE_FILTER": {
       return { ...state, filter: action.payload };
     }
+    case "EDIT_TODO": {
+      const updatedTodoList = state.todoList.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, text: action.payload.text };
+        }
+        return todo;
+      });
+      return { ...state, todoList: updatedTodoList };
+    }
+    case "TOGGLE_TODO": {
+      const updatedTodoList = state.todoList.map((todo) => {
+        if (todo.id === action.payload) {
+          return { ...todo, isCompleted: !todo.isCompleted };
+        }
+        return todo;
+      });
+      return { ...state, todoList: updatedTodoList };
+    }
+    case "REMOVE_TODO": {
+      const updatedTodoList = state.todoList.filter(
+        (todo) => todo.id !== action.payload
+      );
+      return { ...state, todoList: updatedTodoList };
+    }
+    case "SAVE_TO_LOCAL_STORAGE": {
+      return { ...state, todoList: action.payload };
+    }
+    case "CLEAR_COMPLETED_TODOS": {
+      const updatedTodoList = state.todoList.filter(
+        (todo) => !todo.isCompleted
+      );
+      return { ...state, todoList: updatedTodoList };
+    }
 
     default:
       return state;
