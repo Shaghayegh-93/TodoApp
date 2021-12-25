78c95d591da8a218147d1cdb161e4532025c5e8b
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2021-12-25 11:05:36 +0330

    give data as props to TodoList and Footer component

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,14 +1,27 @@
 import Footer from "./Footer";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
+import { useTodoList, useTodoListAction } from "./Context/TodoListProvider";
 
 const TodoApp = () => {
+  const { todoList } = useTodoList();
+  const dispatch = useTodoListAction();
+  const isAllTodosSelected = todoList.every((todo) => todo.isCompleted);
+  const onToggleAllTodos = (e) => {
+    dispatch({ type: "TOGGLE_ALL", payload: e.target.checked });
+  };
   return (
     <div className="todoApp">
       <h1>todos</h1>
       <TodoForm />
-      <TodoList />
-      <Footer />
+      <TodoList
+        isAllTodosSelected={isAllTodosSelected}
+        onToggleAllTodos={onToggleAllTodos}
+      />
+      <Footer
+        isAllTodosSelected={isAllTodosSelected}
+        onToggleAllTodos={onToggleAllTodos}
+      />
     </div>
   );
 };
